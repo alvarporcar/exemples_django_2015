0e314d4684abce198542456ab4fcaeceadeddc9e
alvar porcar <alvar.porcar@gmail.com>
2015-11-25 18:27:39 +0100

    Estudiant aspectes de plantilles ...

--- misitio/urls.py
+++ misitio/urls.py
@@ -1,19 +1,16 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from misitio.views import hola, fecha_actual, horas_adelante, mostrar_navegador, atributos_meta, atributos_meta_template
-from biblioteca import views
-from contactos.views import contactos
 
  
 urlpatterns = [ 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^hola/$', hola), 
-    url(r'^fecha/$', fecha_actual), 
-    url(r'^fecha/mas/(\d{1,2})/$', horas_adelante), 
-    url(r'^navegador/', mostrar_navegador),
-    url(r'^atributos_meta/', atributos_meta),
-    url(r'^atributos_meta_template/', atributos_meta_template),
-    url(r'^formulario_buscar/$', views.formulario_buscar),
-    url(r'^buscar/$', views.buscar),
-    url(r'^contactos/$', contactos),
+    url(r'^hola/$', 'misitio.views.hola'), 
+    url(r'^fecha/$', 'misitio.views.fecha_actual'), 
+    url(r'^fecha/mas/(\d{1,2})/$', 'misitio.views.horas_adelante'), 
+    url(r'^navegador/', 'misitio.views.mostrar_navegador'),
+    url(r'^atributos_meta/', 'misitio.views.atributos_meta'),
+    url(r'^atributos_meta_template/', 'misitio.views.atributos_meta_template'),
+    url(r'^formulario_buscar/$', 'biblioteca.views.formulario_buscar'),
+    url(r'^buscar/$', 'biblioteca.views.buscar'),
+    url(r'^contactos/$', 'contactos.views.contactos'),
 ] 
